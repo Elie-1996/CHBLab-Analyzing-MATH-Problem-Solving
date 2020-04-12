fdf11422d32aea7e5a2678c404667e841294e828
Elie-1996 <44254485+Elie-1996@users.noreply.github.com>
2020-04-12 14:44:28 +0300

    GUI Time Interval Updates
    * User can now choose which coordinates to display according to the time interval

--- GUI.py
+++ GUI.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from Visualization import VisualizationMap
+from warnings import warn
 
 
 def setup_gui(vm: VisualizationMap):
@@ -10,10 +11,10 @@ def setup_gui(vm: VisualizationMap):
     menu_buttons_widget(m, menu_button_frame)
 
     map_frame = Frame()
-    heatmap_widget(map_frame, vm)
+    hm_ax, hm_canvas = heatmap_widget(map_frame, vm)
 
     widgets_frame = Frame()
-    time_widget(widgets_frame)
+    time_widget(widgets_frame, vm, hm_ax, hm_canvas)
 
     menu_button_frame.pack()
     map_frame.pack()
@@ -34,33 +35,68 @@ def menu_buttons_widget(parent, m):
     mb.pack()
 
 
-def heatmap_widget(m, vm):
-    import numpy
-    import matplotlib.pyplot as plt
-    from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-
-    # representation should change but this is a good start for the testing for now :)
-    # real data should be brought here and not some random data as seen below.
-
-    x_coords = []
-    y_coords = []
+def compute_relevant_coordinates(vm):
+    x_coords, y_coords = [], []
     for current_bin in vm.bins:
         for (x, y) in current_bin:
             x_coords.append(x)
             y_coords.append(y)
+    return x_coords, y_coords
+
+
+def heatmap_widget(m, vm):
+    import matplotlib.pyplot as plt
+    from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+    # representation should change but this is a good start for the testing for now :)
+
+    x_coords, y_coords = compute_relevant_coordinates(vm)
+
     f = plt.Figure(figsize=(5, 5), dpi=100)
-    a = f.add_subplot(111)
-    a.imshow(vm.full_image)
-    a.plot(x_coords, y_coords)
+    ax = f.add_subplot(111)
     canvas = FigureCanvasTkAgg(f, m)
     canvas.draw()
     canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=True)
+    substitute_plot(vm, ax, canvas, x_coords, y_coords)
+    return ax, canvas
 
 
-def time_widget(m):
+def substitute_plot(vm, ax, canvas, x_coords, y_coords):
+    print("------------------------------")
+    print(x_coords)
+    print(y_coords)
+    ax.clear()
+    ax.imshow(vm.full_image)
+    ax.plot(x_coords, y_coords)
+    canvas.draw()
+
+
+def attempt_update(vm, ax, canvas, start_time_str, end_time_str):
+    try:
+        start = float(start_time_str)
+        end = float(end_time_str)
+    except ValueError:
+        warn("Could not parse interval correctly: at least start or end interval was incorrectly entered")
+        return
+    vm.update_interval(start, end)
+    x_coords, y_coords = compute_relevant_coordinates(vm)
+    substitute_plot(vm, ax, canvas, x_coords, y_coords)
+
+
+def time_widget(m, vm, ax, canvas):
     Label(m, text="Choose Timeframe (in seconds):").grid(row=1)
 
     Label(m, text='From ').grid(row=2)
     Label(m, text='To: ').grid(row=3)
-    Entry(m).grid(row=2, column=1)
-    Entry(m).grid(row=3, column=1)
+    start_time_interval_entry = Entry(m)
+    start_time_interval_entry.grid(row=2, column=1)
+    end_time_interval_entry = Entry(m)
+    end_time_interval_entry.grid(row=3, column=1)
+    confirmButton = Button(m, text="Confirm", width=16, command=lambda: attempt_update(
+        vm,
+        ax,
+        canvas,
+        start_time_interval_entry.get(),
+        end_time_interval_entry.get()
+    ))
+    confirmButton.grid(row=4, column=1)
